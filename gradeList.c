d5adfe0f38bb879b4932382714adc7831d0ae60d
Daniel Toporovsky <165564571+daniel-toporovsky@users.noreply.github.com>
2024-07-16 16:03:55 +0300

    Add files via upload

---
+++ gradeList.c
@@ -0,0 +1,236 @@
+//#include "/share/ex_data/ex6/gradeList.h"
+//#include "/share/ex_data/ex6/virtualHeap.h"
+/**************************************
+*  Source file for the grade list
+*  code module which maintains a list
+*  of course participants and their grades.
+**************************************/
+#include "gradeList.h"
+#include "virtualHeap.h"
+#include <stdio.h>
+#include <string.h>
+
+/********************************/
+/**      TYPE DEFINITIONS      **/
+/********************************/
+/*** struct type for node in grade list ***/
+typedef struct GradeNode_st GradeNode_st;
+
+/*************************************/
+/**      FUNCTION  DEFINITIONS      **/
+/*************************************/
+
+/*********************************
+* Problem 1.1
+* function newGradeNode
+* params:
+* firstName (char*)
+* lastName  (char*)
+* grade     (int)
+* If grade is out of bounds (not in [0,100]),
+* then do nothing and return NULL
+* Otherwise crate new GradeNode object, which
+* includes the strings for first and last names
+* allocated outside of the struct.
+*********************************/
+GradeNode newGradeNode(const char* firstName, const char* lastName, int grade) {
+    if (grade < 0 || grade > 100) return NULL;
+
+    // Allocating memory for the object
+    GradeNode v;
+    v = ourMalloc(sizeof(GradeNode_st));
+    if (!v) return NULL;
+
+    /*
+     * Allocating memory for the copies of variables
+     * that are stored outside the structure.
+    */
+    v->firstName = ourMalloc(strlen(firstName) * sizeof (char) + 1);
+    v->lastName = ourMalloc(strlen(lastName) * sizeof (char) + 1);
+    if (!v->firstName || !v->lastName) {
+        if (v->firstName) ourFree(v->firstName);
+        if (v->lastName) ourFree(v->lastName);
+        ourFree(v);
+        return NULL;
+    }
+
+    // Copying data to the new object
+    strcpy(v->lastName, lastName);
+    strcpy(v->firstName, firstName);
+    v->grade = (char)grade;
+    v->next = NULL;
+    return v;
+}
+
+/*********************************
+* Problem 1.2
+* function freeGradeList
+* params:
+* gradeList (GradeNode)
+* frees a given grade node and all nodes in
+* the list that is "downstream" from it.
+* frees all memory - the structs and the strings
+* allocated separately
+*********************************/
+void freeGradeList(GradeNode gradeList) {
+    if (!gradeList) return;
+
+    if (gradeList->next) freeGradeList(gradeList->next);
+    ourFree(gradeList->firstName);
+    ourFree(gradeList->lastName);
+    ourFree(gradeList);
+}
+
+/*********************************
+* Problem 1.3
+* function largerGradeNode
+* params:
+* gradeNode1 (GradeNode)
+* gradeNode2 (GradeNode)
+* compares the two grade nodes and
+* returns a pointer to the grade
+* node that is "larger".
+* First compare grades,
+* Then compare last names,
+* Then compare first names.
+* If nodes contain identical
+* information, then returns
+* pointer to gradeNode1
+*********************************/
+GradeNode largerGradeNode(GradeNode gradeNode1, GradeNode gradeNode2) {
+    if (gradeNode1->grade > gradeNode2->grade) return gradeNode1;           // grade1 != grade2
+    else if (gradeNode1->grade < gradeNode2->grade) return gradeNode2;
+    else {                                                                  // grade1 == grade2
+        int lastNameCompare = strcmp(gradeNode1->lastName, gradeNode2->lastName);
+        if (lastNameCompare > 0) return gradeNode1;                         // lastName1 != lastName2
+        else if (lastNameCompare < 0) return gradeNode2;
+        else {                                                              // lastName1 == lastName2
+            int firstNameCompare = strcmp(gradeNode1->firstName, gradeNode2->firstName);
+            if (firstNameCompare >= 0) return gradeNode1;                   // firstName1 >= firstName2
+            else return gradeNode2;
+        }
+    }
+}
+
+/*********************************
+* Problem 1.4
+* function printGradeList
+* params:
+* gradeList (GradeNode)
+* prints a grade list by printing each node
+* in the following format:
+* fistName<TAB>lastName<TAB>grade
+* If list is empty then just prints:
+* Empty grade list
+* Otherwise, after printing all nodes, a summary line is printed:
+* Average of <N> grades is <AVG>
+* where N is the number of grades in the list
+* and <AVG> is the average of grades printed
+* with 1 decimal digit precision
+*********************************/
+void printGradeList(GradeNode gradeList) {
+    if (!gradeList) {
+        printf("Empty grade list\n");
+        return;
+    }
+
+    GradeNode curr = gradeList;
+    double gradeSum = 0;
+    int count = 0;
+    while (curr) {
+        printf("%s\t%s\t%i\n", curr->firstName, curr->lastName, curr->grade);
+        gradeSum += curr->grade;
+        count++;
+        curr = curr->next;
+    }
+    printf("Average of %i grades is %.1f\n" , count, (gradeSum/count));
+}
+
+/*********************************
+* Problem 1.5
+* function appendGradeNode
+* params:
+* gradeList (GradeNode)
+* gradeNode (GradeNode)
+* adds gradeNode to the end of gradeList
+* If gradeNode is not a single node, then does not
+* modify gradeList.
+* Returns the modified list.
+*********************************/
+GradeNode appendGradeNode(GradeNode gradeList, GradeNode gradeNode) {
+    // If the node is not empty and single in the list
+    if (gradeNode && !gradeNode->next) {
+        if (!gradeList) {
+            gradeList = gradeNode;
+        }
+        else {
+            GradeNode curr = gradeList;
+            while (curr->next) curr = curr->next;
+            curr->next = gradeNode;
+        }
+    }
+    return gradeList;
+}
+
+/*********************************
+* Problem 1.6
+* function mergeSortedGradeLists
+* params:
+* gradeList1 (GradeNode)
+* gradeList2 (GradeNode)
+* receives two grade lists sorted from "small"
+* to "large" based on largerGradeNode()
+* and merges them so that sorting is maintained
+* Returns the merged list.
+* Assume input lists are sorted.
+*********************************/
+GradeNode mergeSortedGradeLists(GradeNode gradeList1, GradeNode gradeList2) {
+    // Recursion base
+    if (!gradeList1) return gradeList2;
+    if (!gradeList2) return gradeList1;
+
+    // Starting from the smallest node and merging recursively
+    if (largerGradeNode(gradeList1, gradeList2) == gradeList1) {
+        gradeList2->next = mergeSortedGradeLists(gradeList2->next, gradeList1);
+        return gradeList2;
+    }
+
+    gradeList1->next = mergeSortedGradeLists(gradeList1->next, gradeList2);
+    return gradeList1;
+}
+
+/*********************************
+* Problem 1.7
+* function mergeSortGradeList
+* params:
+* gradeList (GradeNode)
+* sort a given grade list from "small"
+* to "large" based on largerGradeNode()
+* using the merge-sort algorithm
+*********************************/
+GradeNode mergeSortGradeList(GradeNode gradeList) {
+    // Recursion base case ot null input
+    if (!gradeList || !gradeList->next) return gradeList;
+
+    // Counting elements of the Grade List
+    int count = 0;
+    GradeNode curr = gradeList;
+    while (curr) {
+        count++;
+        curr = curr->next;
+    }
+
+    // Splitting Grade List into two
+    curr = gradeList;
+    for (int i = 1; i < count/2; ++i) {
+        curr = curr->next;
+    }
+    GradeNode gradeList1 = gradeList;
+    GradeNode gradeList2 = curr->next;
+    curr->next = NULL;
+
+    // Recursively sort two Grade Lists and merge them together
+    gradeList1 = mergeSortGradeList(gradeList1);
+    gradeList2 = mergeSortGradeList(gradeList2);
+    return mergeSortedGradeLists(gradeList1, gradeList2);
+}
---
+++ processGrades.c
@@ -0,0 +1,173 @@
+//#include "/share/ex_data/ex6/gradeList.h"
+/**************************************
+*  Source file for the processing grades
+*  code module which reads a list of course
+*  participants and their grades from a file,
+*  and then it prints a summary to the standard output.
+**************************************/
+#include "gradeList.h"
+#include <stdio.h>
+#include <string.h>
+
+/*************************************/
+/**       SYMBOLIC CONSTANTS        **/
+/*************************************/
+#define MAX_LINE_LENGTH 200
+
+/*************************************/
+/**      FUNCTION  DECLARATIONS      **/
+/*************************************/
+/*********************************
+* getNextNumberValue
+* params:
+* array (char*)
+* Returns the pointer to
+* the next word in array.
+* If next word or array is empty string returns NULL.
+* Uses static variables to keep information
+* from prev execution
+*********************************/
+char* getNextWord(char* array);
+
+/*********************************
+* getNumberFromString
+* params:
+* string (char*)
+* Returns the value (in type int) of
+* the first number in array.
+* If the 1st word is not a valid number,
+* returns 101.
+*********************************/
+int getNumberFromString(char* string);
+
+int main (int argc, char *argv[]) {
+    // Checking for correct input
+    if (argc == 1) {
+        fprintf (stderr, "Error: no file name provided\n");
+        return 1;
+    }
+    if (argc > 2) {
+        fprintf (stderr, "Error: expecting only one input argument\n");
+        return 2;
+    }
+    FILE *file = fopen(argv[1], "r");
+    if (!file) {
+        fprintf (stderr, "Error: %s cannot be open for reading\n", argv[1]);
+        return 3;
+    }
+
+    // Iterating over file and appending new nodes
+    int exitStatus = 0;
+    GradeNode gradeList = NULL;
+    int line = 0;
+    char input[MAX_LINE_LENGTH+2] = {'\0'};
+    while (fgets(input, MAX_LINE_LENGTH+1, file)) {
+        char *firstName = getNextWord(input);
+        char *lastName = getNextWord(input);
+        int grade = getNumberFromString(getNextWord(input));
+        getNextWord("");            // Resetting global variables
+        line++;
+
+        if (grade > 100 || !firstName || !lastName) {
+            fprintf(stderr, "Line %i has faulty format\n", line);
+            exitStatus = 4;
+        }
+        else {
+            GradeNode curr = newGradeNode(firstName, lastName, grade);
+            if (curr) gradeList = appendGradeNode(gradeList, curr);
+            else {
+                fprintf(stderr, "Out of memory after reading %i lines\n", line);
+                freeGradeList(curr);
+                freeGradeList(gradeList);
+                fclose(file);
+                return 11;
+            }
+        }
+    }
+
+    GradeNode sorted = mergeSortGradeList(gradeList);
+    printGradeList(sorted);
+    freeGradeList(sorted);
+    fclose(file);
+    return exitStatus;
+}
+
+/*************************************/
+/**      FUNCTION  DEFINITIONS      **/
+/*************************************/
+/*********************************
+* getNextWord
+* params:
+* array (char*)
+* Returns the pointer to
+* the next word in array.
+* If next word or array is empty string returns NULL.
+* Uses static variables to keep information
+* from prev execution
+*********************************/
+char* getNextWord(char* array) {
+    static char* prevPointer   = NULL;
+    static char* nextPointer = NULL;
+
+    if (array == prevPointer) {
+        if (!*nextPointer) return NULL;
+
+        array = nextPointer;
+        int i = 0;
+        while (nextPointer[i] != '\n' && nextPointer[i] != ' ' && nextPointer[i]) i++;
+        if (nextPointer[i] == ' ') nextPointer[i++] = '\0';
+        while (nextPointer[i] == ' ') i++;
+        nextPointer = &nextPointer[i];
+        return array;
+    }
+    else {
+        prevPointer = array;
+        if (!*array) return NULL;
+
+        // Skip leading spaces
+        while (*array == ' ') {
+            array++;
+        }
+
+        // Check for empty string after skipping spaces
+        if (!*array) return NULL;
+
+        // Save the next value
+        int i = 0;
+        while (array[i] != '\n' && array[i] != ' ' && array[i]) i++;
+        if (array[i] == ' ') array[i++] = '\0';
+        while (array[i] == ' ') i++;
+        nextPointer = &array[i];
+        return array;
+    }
+}
+
+/*********************************
+* getNumberFromString
+* params:
+* string (char*)
+* Return the value (in type char) of
+* the first number in array.
+* If 1st word is not a valid number,
+* or the array is empty returns 101.
+*********************************/
+int getNumberFromString(char* string) {
+    int value = 0;
+
+    // Check for empty list
+    if (!*string) return 101;
+
+    // If the first character is not a digit the - invalid number
+    if (*string < '0' || *string > '9') return 101;
+
+    // Parse the number
+    while (*string >= '0' && *string <= '9') {
+        value = value * 10 + (*string - '0');
+        string++;
+    }
+
+    // If the next char is invalid - the number is invalid
+    if (*string != '\n' && *string != ' ' && *string) return 101;
+
+    return value;
+}
